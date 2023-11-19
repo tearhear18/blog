17aaa6506c5ed9e18ec4682552e381bee316c2a9
Ramel Cabug-os <tearhear18@gmail.com>
2023-11-19 09:05:08 +0800

    :sparkles: Implement blog crud, blog search, add Gemfile search_cop

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,63 @@
 class BlogsController < ApplicationController
     before_action :authorize_request
 
+    def search
+        blogs = current_user.blog_contents.search params[:q]
+        render json: {
+            message: 'success',
+            blogs: ActiveModel::Serializer::CollectionSerializer.new(
+                blogs,
+                serializer: BlogSerializer
+            )
+        }
+    end
+
+    def index 
+        blogs = current_user.blog_contents
+        render json: {
+            message: 'success',
+            blogs: ActiveModel::Serializer::CollectionSerializer.new(
+                blogs,
+                serializer: BlogSerializer
+            )
+        }
+    end
+    
     def create 
         blog = current_user.blog_contents.create blog_params
-        render json: {message: :blog_created, blog: blog}
+        render json: {
+            message: :blog_created, 
+            blog: BlogSerializer.new(
+				blog
+			),
+        }
+    end
+
+    def show 
+        blog = current_user.blog_contents.find_by_id(params[:id])
+        render json: {
+            message: :blog_created, 
+            blog: BlogSerializer.new(
+				blog
+			),
+        }
+    end
+
+    def update 
+        blog = current_user.blog_contents.find_by_id(params[:id])
+        blog.update blog_params
+        render json: {
+            message: :blog_updated, 
+            blog: BlogSerializer.new(
+				blog
+			),
+        }
+    end
+
+    def destroy 
+        blog = current_user.blog_contents.find_by_id(params[:id])
+        blog.destroy
+        render json: {message: :blog_deleted}
     end
 
     private 
--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -3,8 +3,28 @@ require 'rails_helper'
 RSpec.describe "Blogs", type: :request do
   let(:user){ create(:user)}
   let(:auth){ user.generate_access_token }
-  
-  describe "GET /create" do
+  let!(:blogs){ create_list(:blog_content, 5, user: user) }
+
+  describe "GET /index" do
+    subject do 
+      get blogs_path,
+            as: :json,
+            headers: {
+              HTTP_AUTHORIZATION: "Bearer token=#{auth.token}"
+            }
+    end
+
+    before { subject }
+
+    it do 
+      expect(response).to have_http_status(:ok)
+      res = response.parsed_body
+      expect(res["blogs"].size).to eq(5)
+      expect(res["blogs"].first.keys).to eq(["id", "title", "body"])
+    end
+  end
+
+  describe "POST /create" do
     subject do 
       post blogs_path(params),
             as: :json,
@@ -21,7 +41,115 @@ RSpec.describe "Blogs", type: :request do
       expect(response).to have_http_status(:ok)
       res = response.parsed_body
       expect(res.keys).to eq(["message", "blog"])
+      expect(res["blog"].keys).to eq(["id", "title", "body"])
+    end
+  end
+
+  describe "GET /show" do
+    subject do 
+      get blog_path(params),
+            as: :json,
+            headers: {
+              HTTP_AUTHORIZATION: "Bearer token=#{auth.token}"
+            }
+    end
+
+    let(:params){{id: blogs.pluck(:id).sample}}
+
+    before { subject }
+    it do 
+      expect(response).to have_http_status(:ok)
+      res = response.parsed_body
+      expect(res.keys).to eq(["message", "blog"])
+      expect(res["blog"].keys).to eq(["id", "title", "body"])
     end
 
   end
+
+  describe "PATCH  /update" do 
+    subject do 
+      patch blog_path(params),
+            as: :json,
+            headers: {
+              HTTP_AUTHORIZATION: "Bearer token=#{auth.token}"
+            }
+    end
+
+    let(:params){{id: blogs.pluck(:id).sample, title: "Updated Title", body: "Updated Body"}}
+
+    before { subject }
+    it do 
+      expect(response).to have_http_status(:ok)
+      res = response.parsed_body
+      expect(res.keys).to eq(["message", "blog"])
+      expect(res["blog"]["title"]).to eq("Updated Title")
+      expect(res["blog"]["body"]).to eq("Updated Body")
+    end
+  end
+
+  describe "DELETE  /destroy" do 
+    subject do 
+      delete blog_path(params),
+            as: :json,
+            headers: {
+              HTTP_AUTHORIZATION: "Bearer token=#{auth.token}"
+            }
+    end
+
+    let(:params){{id: blogs.pluck(:id).sample}}
+
+    before { subject }
+    it do 
+      expect(response).to have_http_status(:ok)
+      expect(user.blog_contents.count).to eq(4)
+    end
+  end
+
+  describe "GET  /search" do 
+    subject do 
+      get search_blogs_path(params),
+            as: :json,
+            headers: {
+              HTTP_AUTHORIZATION: "Bearer token=#{auth.token}"
+            }
+    end
+
+    before do 
+      user.blog_contents.create title: "Searchable", body: "Hello World! How wonderful it is"
+    end  
+    
+    context "search with 1 result" do 
+      before { subject }
+      let(:params){{q: "world"}}
+
+      it do 
+        expect(response).to have_http_status(:ok)
+        res = response.parsed_body
+        expect(res["blogs"].size).to eq(1)
+      end
+    end
+
+    context "search with multiple results" do 
+      before { subject }
+      let(:params){{q: "title"}}
+
+      it do 
+        expect(response).to have_http_status(:ok)
+        res = response.parsed_body
+        expect(res["blogs"].size).to eq(5)
+      end
+    end
+
+    context "search with multiple results" do 
+      before { subject }
+      let(:params){{q: "asdsad"}}
+
+      it do 
+        expect(response).to have_http_status(:ok)
+        res = response.parsed_body
+        expect(res["blogs"].size).to eq(0)
+      end
+    end
+  end
+  
 end
